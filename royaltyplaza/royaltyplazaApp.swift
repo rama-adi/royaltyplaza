1d7a89acc6f92510b2e7bbf27d1301508b796902
rama-adi <17980909+rama-adi@users.noreply.github.com>
2015-05-29 00:00:42 +0700

    Initial Commit

---
+++ royaltyplaza/royaltyplazaApp.swift
@@ -0,0 +1,17 @@
+//
+//  royaltyplazaApp.swift
+//  royaltyplaza
+//
+//  Created by Rama Adi Nugraha on 28/05/23.
+//
+
+import SwiftUI
+
+@main
+struct royaltyplazaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
